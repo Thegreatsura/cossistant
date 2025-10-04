a967b33e3da3dc8269eb01521d285502d5104c5d
Rieranthony <rieraanthony13@gmail.com>
2025-10-04 12:21:45 +0300

    feat: remove LRU cache in favour in redis

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -6,34 +6,34 @@ import * as schema from "./schema";
 let _db: NodePgDatabase<typeof schema> | null = null;
 
 const createDb = (): NodePgDatabase<typeof schema> => {
-	if (_db) {
-		return _db;
-	}
+  if (_db) {
+    return _db;
+  }
 
-	_db = drizzle({
-		connection: {
-			host: env.DATABASE_HOST,
-			port: env.DATABASE_PORT,
-			user: env.DATABASE_USERNAME,
-			password: env.DATABASE_PASSWORD,
-			database: env.DATABASE_NAME,
-			ssl:
-				env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-		},
-		cache: bunRedisCache({
-			config: { ex: 60 },
-		}),
-		schema,
-	});
+  _db = drizzle({
+    connection: {
+      host: env.DATABASE_HOST,
+      port: env.DATABASE_PORT,
+      user: env.DATABASE_USERNAME,
+      password: env.DATABASE_PASSWORD,
+      database: env.DATABASE_NAME,
+      ssl:
+        env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    },
+    cache: bunRedisCache({
+      config: { ex: 600 },
+    }),
+    schema,
+  });
 
-	return _db;
+  return _db;
 };
 
 export type Database = ReturnType<typeof drizzle<typeof schema>>;
 
 export const db = new Proxy({} as Database, {
-	get: (target, prop) => {
-		const actualDb = createDb();
-		return actualDb[prop as keyof typeof actualDb];
-	},
+  get: (target, prop) => {
+    const actualDb = createDb();
+    return actualDb[prop as keyof typeof actualDb];
+  },
 });
