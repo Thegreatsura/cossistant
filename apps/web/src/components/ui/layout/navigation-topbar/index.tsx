c89080df625c9c599fa3e58533aa9935ef8d4d6f
Rieranthony <rieraanthony13@gmail.com>
2025-10-06 15:23:20 +0300

    feat: better dashboard ux

--- apps/web/src/components/ui/layout/navigation-topbar/index.tsx
+++ apps/web/src/components/ui/layout/navigation-topbar/index.tsx
@@ -8,43 +8,38 @@ import { Logo } from "../../logo";
 import { TopbarItem } from "./topbar-item";
 
 export function NavigationTopbar() {
-	const pathname = usePathname();
-	const website = useWebsite();
+  const pathname = usePathname();
+  const website = useWebsite();
 
-	const baseInboxPath = `/${website?.slug}/inbox`;
+  const baseInboxPath = `/${website?.slug}/inbox`;
 
-	return (
-		<header className="flex h-16 min-h-16 w-full items-center justify-between gap-4 pr-3 pl-6">
-			<div className="flex flex-1 items-center gap-4">
-				<Link href={baseInboxPath}>
-					<Logo className="size-6 text-primary" />
-				</Link>
-				<TopbarItem
-					active={pathname.includes(baseInboxPath)}
-					href={baseInboxPath}
-					iconName="inbox-zero"
-				>
-					{website?.name}
-				</TopbarItem>
-				<TopbarItem
-					active={pathname === `/${website?.slug}/agents`}
-					hideLabelOnMobile
-					href={`/${website?.slug}/agents`}
-					iconName="agent"
-				>
-					Agents
-				</TopbarItem>
-			</div>
-			<div className="flex items-center gap-3">
-				<TopbarItem
-					active={pathname === `/${website?.slug}/settings`}
-					hideLabelOnMobile
-					href={`/${website?.slug}/settings`}
-					iconName="settings"
-				>
-					Settings
-				</TopbarItem>
-			</div>
-		</header>
-	);
+  return (
+    <header className="flex h-16 min-h-16 w-full items-center justify-between gap-4 pr-3 pl-6.5">
+      <div className="flex flex-1 items-center gap-4">
+        <Link href={baseInboxPath}>
+          <Logo className="size-6 text-primary" />
+        </Link>
+        <TopbarItem
+          active={pathname.includes(baseInboxPath)}
+          href={baseInboxPath}
+          iconName="inbox-zero"
+        >
+          {website?.name}
+        </TopbarItem>
+        {/* <TopbarItem
+          active={pathname === `/${website?.slug}/agents`}
+          hideLabelOnMobile
+          href={`/${website?.slug}/agents`}
+          iconName="agent"
+        >
+          Agents
+        </TopbarItem> */}
+      </div>
+      <div className="flex items-center gap-3">
+        <TopbarItem external hideLabelOnMobile href={"/docs"}>
+          Docs
+        </TopbarItem>
+      </div>
+    </header>
+  );
 }
