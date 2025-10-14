d61e23c95970ff3c949b58762c376c748efcd5b0
Rieranthony <rieraanthony13@gmail.com>
2025-10-14 23:31:52 +0200

    feat: better doc structure

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,7 +1,9 @@
 "use client";
 
 export * from "./hooks";
+export * from "./identify-visitor";
 export * from "./primitives";
 export * from "./provider";
 export * from "./realtime";
 export * from "./support";
+export * from "./support-config";
