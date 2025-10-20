f62cf646e9bcaba7e4cdbdcdefb9c366cf511725
Rieranthony <rieraanthony13@gmail.com>
2025-10-20 13:38:05 +0200

    feat: fix radix issues

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -10,62 +10,63 @@ import { generateSiteMetadata } from "@/lib/metadata";
 import { Providers } from "./providers";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-	display: "swap",
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-	display: "swap",
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const F37Stout = localFont({
-	src: [
-		{
-			path: "../../public/fonts/F37Stout-Regular.woff2",
-			weight: "400",
-			style: "normal",
-		},
-	],
-	variable: "--font-f37-stout",
-	display: "swap",
+  src: [
+    {
+      path: "../../public/fonts/F37Stout-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-f37-stout",
+  display: "swap",
 });
 
 export const metadata = generateSiteMetadata();
 
 export const viewport = {
-	width: "device-width",
-	initialScale: 1,
-	maximumScale: 1,
-	userScalable: false,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)" },
-		{ media: "(prefers-color-scheme: dark)" },
-	],
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)" },
+    { media: "(prefers-color-scheme: dark)" },
+  ],
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} ${F37Stout.variable} group/body overscroll-none antialiased`}
-			>
-				<Script
-					data-domain="cossistant.com"
-					data-website-id="68dfcb6e6f762f4ecaf58bb4"
-					src="https://datafa.st/js/script.js"
-					strategy="afterInteractive"
-				/>
-				<Providers>{children}</Providers>
-				<Toaster />
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${F37Stout.variable} group/body antialiased`}
+      >
+        <div className="isolate" id="radix-portal-root" />
+        <Script
+          data-domain="cossistant.com"
+          data-website-id="68dfcb6e6f762f4ecaf58bb4"
+          src="https://datafa.st/js/script.js"
+          strategy="afterInteractive"
+        />
+        <Providers>{children}</Providers>
+        <Toaster />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
