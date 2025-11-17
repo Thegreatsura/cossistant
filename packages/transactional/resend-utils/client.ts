3c9e227342a1d3ac415a9166da549477353ffe2c
Rieranthony <rieraanthony13@gmail.com>
2025-11-17 12:41:18 +0100

    chore: add linkifyjs dependency to package.json and bun.lock, and fix Vercel build issue in resend client

--- packages/transactional/resend-utils/client.ts
+++ packages/transactional/resend-utils/client.ts
@@ -2,4 +2,7 @@ import { Resend } from "resend";
 
 const apiKey = process.env.RESEND_API_KEY;
 
-export const resend = apiKey ? new Resend(apiKey) : undefined;
+// This is fix vercel build
+export const resend = (apiKey
+	? new Resend(apiKey)
+	: undefined) as unknown as Resend;
